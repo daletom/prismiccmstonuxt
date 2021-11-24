a09043b0d57e3c8ef79cf17caf9eb08c16502185
Tom Dale <thomas.dale2@gmail.com>
2021-11-24 09:22:17 -0800

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="footer">
+    <a href="https://www.letsgoheadless.com" target="_blank" rel="noopener">
+      <img class="logo" src="https://tom.imgix.net/letsgoheadless_logo_mobile.png?auto=format,compress&w=300&ch=dpr,width" alt="Let's Go Headless">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.footer {
+    padding: 40px 0;
+  max-width: 700px;
+  margin: 0 auto;
+  text-align: center;
+  border-top: 1px solid #DADADA
+}
+.logo {
+    width: 300px;
+    display: inline-block;
+}
+</style>
